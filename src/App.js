55cfea6dd43194245ad917cd4d8b464bc6ec1741
laroiba <tomato.202404.nahda@gmail.com>
2024-05-08 14:48:28 +0900

    Edited App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-     hello wiorld
-    </div>
-  );
+  return <div className="App">Good Morning</div>;
 }
 
 export default App;
