a139fb54d920409521b46751781d6f164ad46e5c
hlanjuyeon <gkswndus135@gmail.com>
2024-05-16 17:27:39 +0900

    5.16

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -273,6 +273,7 @@ export const Main = (props) => {
                     </List>
                 </Container>
             </StyledContainer>
+            <div>zzzzzzzzzzzzzzzzzzzzzz</div>
         </>
     );
 };
